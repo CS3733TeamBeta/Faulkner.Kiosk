f784aa833d0d8ad36f8b5f7f6ae4c4c34c07ffb1
pjpolley <pjpolley@wpi.edu>
2017-01-31 18:58:15 -0500

    Add baseline for scene transition. will finalize tonight

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -11,16 +11,20 @@ import java.net.URL;
 
 public class Main extends Application {
 
+    public static Stage thisStage;
+    public static Scene scene1, scene2;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        URL url = getClass().getResource("../DragWindow.fxml");
-
-        Parent root = FXMLLoader.load(url);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        thisStage = primaryStage;
+        Parent scene1root = FXMLLoader.load(getClass().getResource("../AdminLoginView.fxml"));
+        Parent scene2root = FXMLLoader.load(getClass().getResource("../AdminWelcomeView.fxml"));
+        thisStage.setTitle("Hello World");
+        scene1 = new Scene(scene1root);
+        scene2 = new Scene(scene2root);
+        thisStage.setScene(scene1);
+        thisStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
