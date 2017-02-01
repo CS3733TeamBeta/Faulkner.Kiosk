f9cc3c976702ea1a8787c6a210a4626ca108e138
Joan Wong <jwong3@wpi.edu>
2017-01-31 16:46:06 -0800

    Added AddNewProfile and ChooseProfileToModify scenes with their controllers

---
+++ src/Controller/profileToModifyController.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ScrollPane;
+
+public class profileToModifyController {
+    @FXML
+    Button logout, back;
+
+    @FXML
+    TextField searchModDoc;
+
+    @FXML
+    ScrollPane filteredProfiles;
+}
