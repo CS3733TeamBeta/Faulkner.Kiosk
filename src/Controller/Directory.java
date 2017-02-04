5548b0f8a0164932ef0bbc91d46f4686f230d74b
Joan Wong <jwong3@wpi.edu>
2017-02-04 00:57:20 -0800

    Updated Directory class

--- src/Controller/Directory.java
+++ src/Controller/Directory.java
@@ -1,30 +1,32 @@
 package Controller;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 /**
  * Created by jw97 on 2/3/2017.
  */
 public class Directory {
-    HashSet<doctorProfile> doctors = new HashSet<doctorProfile>();
+    HashMap<doctorProfile, roomInfo> doctors = new HashMap<doctorProfile, roomInfo>();
 
     public Directory() {
         doctors.clear();
     }
 
-    public int addToDirectory(doctorProfile doctor) {
-        if (this.doctors.add(doctor)) {
-            return 0; // Adds the doctor to the directory
-        } else {
+    public int addToDirectory(doctorProfile doctor, roomInfo room) {
+        if (this.doctors.containsKey(doctor)){
             return 1; // Exception
         }
+
+        this.doctors.put(doctor, room);
+        return 0;
     }
 
     public int removeFromDirectory(doctorProfile doctor) {
-        if (this.doctors.remove(doctor)) {
-            return 0; // Removes the doctor from the director if it is present
-        } else {
-            return 1; // Exception
+        if (this.doctors.containsKey(doctor)) {
+            this.doctors.remove(doctor);
+            return 0;
         }
+
+        return 1; // Exception
     }
 }
