ac9bacdb140749ab8ae5ae81048597b75fde476b
Joan Wong <jwong3@wpi.edu>
2017-02-03 21:11:48 -0800

    Edited the addNewProfile controller file

--- src/Controller/addNewProfileController.java
+++ src/Controller/addNewProfileController.java
@@ -5,29 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 
 public class addNewProfileController {
     @FXML
-    Button logout, back, save, assignLoc; // assignLoc should bring up a map with nodes (admin could click and assign)
+    Button logout, back, save;
+
+    @FXML
+    Button add, remove, deselectAll, selectAll;
 
     @FXML
     TextField firstName, lastName;
 
     @FXML
+    TextField roomNum;
+
+    @FXML
+    ListView deptList;
+
+    @FXML
     TextArea deptIn; // Shows the list of department(s) in which the doctor is in
 
-    @FXML
-    ScrollBar scrollDeptIn; // Allows you to scroll through the department(s) added to this doctor
-
-    @FXML
-    MenuButton addDeptMenu; // List of departments to add to the doctor's profile
-
-    @FXML
-    Label output; //This displays the room in which the doctor is assigned to (show which room has been processed)
 
     @FXML
     ImageView profilePic;
