84ad5d6d370b8591c886990a773a0268a69bcc04
pjpolley <pjpolley@wpi.edu>
2017-02-03 23:50:48 -0500

    Add logic for ending the search loop and the ability to pop the node with the best heuristic from a list of nodes.

--- src/Domain/Navigation/Path.java
+++ src/Domain/Navigation/Path.java
@@ -14,19 +14,37 @@ public class Path implements Iterable
 
     public Path (Destination start, Destination end) {
 
+        boolean flagDone = false;
+
         LinkedList<MapNode> openSet = new LinkedList<MapNode>();
         LinkedList<MapNode> closedSet = new LinkedList<MapNode>();
 
         openSet.add(start);
 
-        while (openSet.size() > 0) {
-            MapNode currentNode = openSet.getFirst();
+        while (openSet.size() > 0 && !flagDone) {
+            MapNode currentNode = popSmallest(openSet);
 
             openSet.remove(0);
             closedSet.add(currentNode);
 
             if (currentNode.equals(end)) {
-                //do something
+                //return the path from start to end
+                LinkedList<NodeEdge> invertedList = new LinkedList<>();
+                MapNode currentInPath = currentNode;
+                int length = 0;
+                while(currentInPath.getParent() != null){
+                    invertedList.add(currentInPath.getParent());
+                    currentInPath = currentInPath.getParent().getOtherNode(currentInPath);
+                    length++;
+                }
+
+                //now invert the inverted list to get the path from start to finish:
+                for(int i = 0; i < length; i++){
+                    pathEdges.add(length - i - 1, invertedList.pop());
+                }
+
+                flagDone = true;
+
             }
 
             for (NodeEdge aEdge: currentNode.edges) {
@@ -39,11 +57,7 @@ public class Path implements Iterable
             }
 
         }
-
-
-        //@TODO: calculate path with A*
-
-        //When done, set all nodes back to default values
+        //TODO: When done, set all nodes back to default values
     }
 
     public static double findHeuristic(MapNode currentNode, MapNode endNode){
@@ -52,6 +66,18 @@ public class Path implements Iterable
         double endNodeX = (double)endNode.getPosX();
         double endNodeY = (double)endNode.getPosY();
         return Math.sqrt(Math.pow(endNodeY - currentNodeY, 2) + Math.pow(endNodeX - currentNodeX, 2));
+    }
+
+    public static MapNode popSmallest(LinkedList<MapNode> listOfNodes){
+        MapNode smallestNode = new MapNode();
+        for(int i = 0; i < listOfNodes.size(); i++){
+            if(listOfNodes.get(i).getF() < smallestNode.getF()){
+                smallestNode = listOfNodes.get(i);
+            }
+        }
+        listOfNodes.remove(smallestNode);
+        return smallestNode;
+
     }
 
 
