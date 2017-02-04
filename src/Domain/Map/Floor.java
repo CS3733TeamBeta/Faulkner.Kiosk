083517107c9d95ca5ea7c62586b635e95bcedacd
Ian Johnson <icjohnson@wpi.edu>
2017-02-03 21:37:00 -0500

    Flesheding out constructors and helpers

--- src/Domain/Map/Floor.java
+++ src/Domain/Map/Floor.java
@@ -5,8 +5,20 @@ import java.util.LinkedList;
 /**
  * Represents a floor in a building. A floor will have nodes, node edges, destinations, more
  */
-public class Floor
-{
+public class Floor {
     LinkedList<MapNode> floorNodes;
     LinkedList<NodeEdge> floorEdges;
+
+    public Floor() {
+        floorNodes = new LinkedList<MapNode>();
+        floorEdges = new LinkedList<NodeEdge>();
+    }
+
+    public void addNode(MapNode n) {
+        floorNodes.add(n);
+    }
+
+    public void addEdge(NodeEdge e) {
+        floorEdges.add(e);
+    }
 }
