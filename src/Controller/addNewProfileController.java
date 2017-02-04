ce0d3546036ddc41a8f16bc1e28d3fa006771de6
Joan Wong <jwong3@wpi.edu>
2017-02-04 03:21:56 -0800

    Added new methods (addressing the selection of departments) in the addNewProfileController file

--- src/Controller/addNewProfileController.java
+++ src/Controller/addNewProfileController.java
@@ -1,34 +1,51 @@
+// Create and edit by Joan Wong
+
 package Controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 public class addNewProfileController {
+    Boolean noEmptyFields = true; // Checks if the fields are empty; true, for testing purposes
+    ObservableList<String> addedDept = FXCollections.observableArrayList();
+    ObservableList<String> deptList = FXCollections.observableArrayList();
+
     @FXML
     Button logout, back, save;
 
     @FXML
-    Button add, remove, deselectAll, selectAll;
+    Button add;
 
     @FXML
-    TextField firstName, lastName;
+    Button remove;
+
+    @FXML
+    Button showDeptList;
+
+    @FXML
+    TextField firstName;
+
+    @FXML
+    TextField lastName;
 
     @FXML
     TextField roomNum;
 
     @FXML
-    ListView deptList;
+    ListView<String> deptListView;
 
     @FXML
-    TextArea deptAdded; // Shows the list of department(s) in which the doctor is in
+    ListView<String> deptAddedListView;
 
     @FXML
     ImageView profilePic;
+
 
     @FXML
     private void logoutHit(){
@@ -42,13 +59,52 @@ public class addNewProfileController {
 
     @FXML
     private void saveHit(){
+        if (noEmptyFields) {
+            doctorProfile newDoctor = new doctorProfile(firstName.getText(), lastName.getText());
+            System.out.println("You have added a new profile of name: " + newDoctor.getFirstName() + " "
+                    + newDoctor.getLastName());
+            newDoctor.assignRoom(roomNum.getText());
+            System.out.println("You have assigned him/her to this room:" + newDoctor.getRoomNum());
+
+        } else {
+            return; // Exception
+        }
+
         Main.thisStage.setScene(Main.changingDirectoryView);
     }
 
     @FXML
-    private void assignLocHit(){
-        //where does this go...?
+    private void showDeptList() {
+        deptList = Main.departments;
+        deptListView.setItems(FXCollections.observableList(deptList));
+
     }
 
+    @FXML
+    private void addClicked() {
+        String deptSelected = deptListView.getSelectionModel().getSelectedItem();
 
+        if (deptSelected != null) {
+            deptListView.getSelectionModel().clearSelection();
+            addedDept.add(deptSelected);
+        } else {
+            return;
+        }
+
+        deptAddedListView.setItems(FXCollections.observableList(addedDept));
+    }
+
+    @FXML
+    private void removeClicked() {
+        String deptToBeRemoved = deptAddedListView.getSelectionModel().getSelectedItem();
+
+        if (deptToBeRemoved != null) {
+            deptAddedListView.getSelectionModel().clearSelection();
+            addedDept.remove(deptToBeRemoved);
+        } else {
+            return;
+        }
+
+        deptAddedListView.setItems(FXCollections.observableList(addedDept));
+    }
 }
