fc73e31afeff70cc8fda42f1c2edd82e72bf1d8b
Ian Johnson <icjohnson@wpi.edu>
2017-02-03 20:50:13 -0500

    Setting up Astar

--- src/Domain/Map/MapNode.java
+++ src/Domain/Map/MapNode.java
@@ -13,11 +13,20 @@ public class MapNode
     int posX;
     int posY;
     int nodeID;
-
+    int g;
+    int heuristic;
+    int f = Integer.MAX_VALUE;
+    MapNode parent = null;
     Image node = null;
 
     Floor myFloor;
     public HashSet<NodeEdge> edges;
+
+
+
+    public MapNode() {
+
+    }
 
     public boolean equals(Object obj) {
         if (obj instanceof MapNode) {
