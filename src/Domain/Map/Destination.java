c716c2dad024d970e3002485b2cac211be14df2d
Joan Wong <jwong3@wpi.edu>
2017-02-07 20:21:53 -0800

    Removed comments

--- src/Domain/Map/Destination.java
+++ src/Domain/Map/Destination.java
@@ -8,12 +8,11 @@ import sun.security.krb5.internal.crypto.Des;
  */
 
 public class Destination extends MapNode {
-    /* Info myInfo;
+    Info myInfo;
     Image icon;
     Image destinationView;
 
     public Destination() {
         myInfo = new Info("1", "2", "3");
     }
-    */
 }
