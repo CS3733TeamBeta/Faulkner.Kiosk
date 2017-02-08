f26e984cc72650e3e0437654aa47c88ca3756f27
Brandon Knox <bdknox@wpi.edu>
2017-02-07 22:18:46 -0500

    Restored deleted Doctor and Offie

---
+++ src/Domain/Map/Office.java
@@ -0,0 +1,36 @@
+package Domain.Map;
+
+import javax.persistence.*;
+import java.util.HashSet;
+
+/**
+ *  Office is a particular type of destination that is a doctor's office
+ */
+
+public class Office extends Destination
+{
+    int id;
+    HashSet<Doctor> occupants;
+
+    public Office(int id, HashSet<Doctor> doctors) {
+        this.id = id;
+        this.occupants = doctors;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public HashSet<Doctor> getOccupants() {
+        return this.occupants;
+    }
+
+    public void setOccupants(HashSet<Doctor> doctors) {
+        this.occupants = doctors;
+    }
+
+}
