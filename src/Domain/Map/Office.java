ecb5ec7a9e6a20ca7d4e487a5da8d6bcfdc9de4a
Ian Johnson <icjohnson@wpi.edu>
2017-02-15 13:36:21 -0500

    Javadocd Office

--- src/Domain/Map/Office.java
+++ src/Domain/Map/Office.java
@@ -17,46 +17,78 @@ public class Office extends Destination
     protected String department;
     private final String popOverEditFXML = "/Admin/Popup/OfficeEditPopup.fxml";
 
+    /**
+     * Creates a new Office with the given ID and the given doctors
+     * @param id as an integer
+     * @param doctors as a HashSet<Doctor>
+     */
     public Office(int id, HashSet<Doctor> doctors) {
         this.id = id;
         this.occupants = doctors;
     }
 
-    public Office()
-    {
-
+    /**
+     * Create an empty office
+     */
+    public Office() {
     }
 
+    /**
+     * Retrieves the popover contained within this?
+     * @TODO Make this comment
+     * @return
+     */
     @Override
-    public PopOver getEditPopover()
-    {
+    public PopOver getEditPopover() {
         OfficeEditController controller = new OfficeEditController(this);
-
         return getPopOver(controller, popOverEditFXML);
     }
+
+    /**
+     * Retrieves this office's ID
+     * @return this office's ID, as an int
+     */
     public int getId() {
         return this.id;
     }
 
+    /**
+     * Sets this office's ID to the given ID
+     * @param id as an int
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Retrieves the occupants of this office as a collection of doctors
+     * @return the doctors in this office, as a HashSet<Doctor>
+     */
     public HashSet<Doctor> getOccupants() {
         return this.occupants;
     }
 
+    /**
+     * Sets this Office's HashSet of doctors to the given HashSet
+     * @param doctors the desired HashSet of docotrs
+     */
     public void setOccupants(HashSet<Doctor> doctors) {
         this.occupants = doctors;
     }
 
-    public void setDepartment(String department)
-    {
+    /**
+     * Sets the department of this office to the given department
+     * @param department as a String
+     */
+    public void setDepartment(String department) {
         this.department = department;
     }
 
-    public String getDepartment()
-    {
+    /**
+     * Retrieves the department of this office
+     * @return the department of this office, as a String
+     */
+    public String getDepartment() {
         return this.department;
     }
 }
