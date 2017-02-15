e4d35f7091663fa9851e34490acc0c5709dac3a9
Ian Johnson <icjohnson@wpi.edu>
2017-02-15 17:56:47 -0500

    Added a class to send emails

---
+++ src/Controller/SendEmail.java
@@ -0,0 +1,54 @@
+package Controller;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+public class SendEmail {
+    public static void main(String[] args) {
+        final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+        // Get a Properties object
+        Properties props = System.getProperties();
+        props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "465");
+        props.setProperty("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.store.protocol", "pop3");
+        props.put("mail.transport.protocol", "smtp");
+        final String username = "faulknerkioskdirections@gmail.com";//
+        final String password = "FaulkPassword";
+        try {
+            Session session = Session.getDefaultInstance(props,
+                    new Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+
+            // -- Create a new message --
+            Message msg = new MimeMessage(session);
+
+            String recipient = "iancj97@gmail.com";
+            // -- Set the FROM and TO fields --
+            msg.setFrom(new InternetAddress(username));
+            msg.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(recipient, false));
+            msg.setSubject("Hello");
+            msg.setText("How are you");
+            msg.setSentDate(new Date());
+            Transport.send(msg);
+            System.out.println("Message sent.");
+        } catch (MessagingException e) {
+            System.out.println("Erreur d'envoi, cause: " + e);
+        }
+    }
+}
