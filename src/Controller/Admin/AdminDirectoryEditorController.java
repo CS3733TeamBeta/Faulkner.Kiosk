5e3c2674ed7a1954543e80dedafc637afdb4f829
Devon Bray <dev@esologic.com>
2017-02-16 16:33:46 -0500

    Can't get adding to table working

--- src/Controller/Admin/AdminDirectoryEditorController.java
+++ src/Controller/Admin/AdminDirectoryEditorController.java
@@ -3,12 +3,23 @@ package Controller.Admin;
 import Controller.AbstractController;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXTreeTableColumn;
 import com.jfoenix.controls.JFXTreeTableView;
 import javafx.fxml.FXML;
-import javafx.stage.Stage;
+import javafx.scene.control.TreeTableColumn;
 
+import java.util.List;
 
-public class AdminDirectoryEditorController extends AbstractController {
+public class AdminDirectoryEditorController  extends AbstractController{
+
+    public AdminDirectoryEditorController()
+    {
+        JFXTreeTableColumn<?, ?> deptColumn = new JFXTreeTableColumn<>("Thing ");
+
+        directoryTable.setShowRoot(false);
+        directoryTable.setEditable(true);
+        directoryTable.getColumns().add(0, deptColumn);
+    }
 
     @FXML
     private JFXTreeTableView<?> directoryTable;
@@ -17,10 +28,25 @@ public class AdminDirectoryEditorController extends AbstractController {
     private JFXTextField nameTextField;
 
     @FXML
-    private JFXTextField phoneNumberTextField;
+    private JFXTextField phoneNumberOneTextField;
+
+    @FXML
+    private JFXTextField phoneNumberTwoTextField;
+
+    @FXML
+    private JFXTextField phoneNumberThreeTextField;
 
     @FXML
     private JFXTreeTableView<?> departmentsTable;
+
+    @FXML
+    private JFXTextField officeNumberTextField;
+
+    @FXML
+    private JFXTextField hoursStartTimeTextField;
+
+    @FXML
+    private JFXTextField hoursEndTimeTextField;
 
     @FXML
     private JFXButton saveButton;
@@ -28,14 +54,9 @@ public class AdminDirectoryEditorController extends AbstractController {
     @FXML
     private JFXButton newButton;
 
-    public AdminDirectoryEditorController(){
-
-    }
-
     @FXML
-    void clickedSave() {
-        System.out.print("nameTextField: ");
-        System.out.print(nameTextField.getText());
+    void clickedSave()
+    {
+        System.out.println("Thing");
     }
-
 }
