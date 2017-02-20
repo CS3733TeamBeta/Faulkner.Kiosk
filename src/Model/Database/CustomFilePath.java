015e19491e6ef1042c8877217df13bdb0f304e34
Joan Wong <jwong3@wpi.edu>
2017-02-19 22:38:24 -0800

    made commit changes

--- src/Model/Database/CustomFilePath.java
+++ src/Model/Database/CustomFilePath.java
@@ -4,7 +4,7 @@ package Model.Database;
  * Created by IanCJ on 2/19/2017.
  */
 public class CustomFilePath {
-    public static final String myFilePath = "jdbc:derby:/";
+    public static final String myFilePath = "jdbc:derby:/Users/jw97/";
 
     CustomFilePath() {
 
