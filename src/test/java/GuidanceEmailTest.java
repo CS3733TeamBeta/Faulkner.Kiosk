db1b2dfc61f3c98df42d3c9a0457ef61c421cde6
Ian Johnson <icjohnson@wpi.edu>
2017-02-20 14:43:47 -0500

    moving test cases around

--- src/test/java/GuidanceEmailTest.java
+++ src/test/java/GuidanceEmailTest.java
@@ -1,15 +1,12 @@
-package test.java;
+package java;
 
 import Domain.Map.*;
 import Domain.Navigation.Guidance;
 import Domain.Navigation.Path;
-import Domain.Navigation.*;
 import Exceptions.PathFindingNoPathException;
 import junit.framework.TestCase;
 
-import java.rmi.server.UID;
 import java.util.LinkedList;
-import java.util.UUID;
 
 
 /**
@@ -18,7 +15,7 @@ import java.util.UUID;
  * Jesus christ fuck this test
  *
  */
-public class PathFindingTest extends TestCase{
+public class GuidanceEmailTest extends TestCase{
 
     MapNode nodeA, nodeB, nodeC, nodeD, nodeE, nodeF, nodeG, nodeH, nodeI, nodeJ, nodeK, nodeL, nodeM, nodeN, nodeO, nodeP, nodeQ, nodeR, nodeS, nodeT, nodeX, nodeY, nodeZ;
     NodeEdge edgeAB, edgeBC, edgeCD, edgeBE, edgeCF, edgeDG, edgeEJ, edgeGK, edgeAH, edgeHI, edgeIJ, edgeJK, edgeLM, edgeMN, edgeMP, edgeNQ, edgeOP, edgePZ, edgePS, edgeQT, edgeLR, edgeRS, edgeST, edgeYX, edgeEZ;
@@ -118,100 +115,13 @@ public class PathFindingTest extends TestCase{
         myHospital.addBuilding(myBuilding);
     }
 
-    //Tests that UIDs are working properly
-    public void testUID() {
-
-        assertEquals(nodeA, nodeA);
-        assertEquals(nodeB, nodeB);
-        assertNotSame(nodeA, nodeB);
-    }
-
-    //Tests that IsValidPath is properly testing path validity
-    public void testIsValidPath() {
-        LinkedList<MapNode> listOfNodes = new LinkedList<MapNode>();
-        LinkedList<MapNode> listOfInvalidNodes = new LinkedList<MapNode>();
-        listOfNodes.add(nodeA);
-        listOfNodes.add(nodeB);
-        listOfInvalidNodes.add(nodeA);
-        listOfInvalidNodes.add(nodeE);
-        LinkedList<NodeEdge> listOfEdges = new LinkedList<NodeEdge>();
-        listOfEdges.add(edgeAB);
-        Path manPathValid = new Path(listOfEdges, listOfNodes);
-        Path manPathInvalid = new Path(listOfEdges, listOfInvalidNodes);
-        assertTrue(manPathValid.isValidPath());
-        assertFalse(manPathInvalid.isValidPath());
-    }
-
-
-    public void testNoValidPathException() {
+    public void testSendEmail() {
         try {
-            Path p = new Path(nodeA, nodeX);
-            fail("Failed to notice no possible path");
+            Guidance g1 = new Guidance(nodeA, nodeL, false);
+            //Please don't uncomment this line unless you replace the email with your own. I'm getting a lot of spam -Ian
+            //g1.sendEmailGuidance("iancj97@gmail.com");
         } catch (Exception e) {
-            assertTrue(e instanceof PathFindingNoPathException);
-        }
-        try {
-            Path p = new Path(nodeY, nodeL);
-            fail("Failed to notice no possible path");
-        } catch (Exception e) {
-            assertTrue(e instanceof PathFindingNoPathException);
-        }
-
-    }
-
-    public void testEquals() {
-        try {
-            p1 = new Path(nodeA, nodeL);
-            p2 = new Path(nodeA, nodeL);
-            p3 = new Path(nodeL, nodeA);
-            p4 = new Path(nodeB, nodeD);
-            p5 = new Path(nodeA, nodeD);
-            assertTrue(p1.equals(p2));
-            assertTrue(p2.equals(p1));
-            assertFalse(p1.equals(p3));
-            assertFalse(p3.equals(p1));
-            assertFalse(p4.equals(p5));
-            assertFalse(p5.equals(p4));
-            assertFalse(p3.equals(p5));
-            assertFalse(p5.equals(p3));
-        } catch(Exception e) {
-            fail("Threw unexpected exception in testEquals");
+            fail("Threw exception");
         }
     }
-
-    public void testGuidance() {
-        try {
-            g1 = new Guidance(nodeT, nodeH, false);
-            g2 = new Guidance(nodeA, nodeL, false);
-            g2.printTextDirections();
-            assertTrue(true);
-       } catch(Exception e) {
-            fail("Threw unexpected exception in testGuidance");
-        }
-    }
-
-    public void testBreadth() {
-       try {
-            p1 = new Path(nodeA, nodeL, true, "breadthfirst");
-        } catch (Exception e) {
-            fail("Threw unexpected exception in testBreadth");
-        }
-    }
-
-    public void testDepth() {
-        try {
-            p1 = new Path(nodeA, nodeL, true, "depthfirst");
-        } catch (Exception e) {
-            fail("Threw unexpected exception in testDepth");
-        }
-    }
-
-    public void testRandom() {
-        try {
-            p1 = new Path(nodeA, nodeL, true, "random");
-        } catch (Exception e) {
-            fail("Threw unexpected exception in testRandom");
-        }
-    }
-    
 }
