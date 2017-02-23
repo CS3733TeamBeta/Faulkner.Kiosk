56ee31ee59b73d7a191f9c6d1f3b736baba163c2
Ian Johnson <icjohnson@wpi.edu>
2017-02-23 04:27:27 -0500

    forgot to add file

---
+++ src/Domain/Navigation/DirectionStep.java
@@ -0,0 +1,40 @@
+package Domain.Navigation;
+
+import Domain.Map.NodeEdge;
+
+import java.util.LinkedList;
+
+/**
+ * Created by IanCJ on 2/23/2017.
+ */
+public class DirectionStep {
+    LinkedList<NodeEdge> stepEdges;
+    String instruction;
+
+    public DirectionStep(String instruction){
+        this.instruction = instruction;
+        this.stepEdges = new LinkedList<>();
+    }
+
+    public DirectionStep(String instruction, LinkedList<NodeEdge> stepEdges) {
+        this.instruction = instruction;
+        this.stepEdges = stepEdges;
+    }
+
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+    public String getInstruction() {
+        return this.instruction;
+    }
+    public void setStepEdges(LinkedList<NodeEdge> stepEdges) {
+        this.stepEdges = stepEdges;
+    }
+    public LinkedList<NodeEdge> getStepEdges() {
+        return this.stepEdges;
+    }
+
+    public String toString() {
+        return this.instruction;
+    }
+}
