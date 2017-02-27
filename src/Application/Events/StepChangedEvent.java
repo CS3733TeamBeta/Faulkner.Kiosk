67f6dae868e69540bd8c7f4c1c2a8c2c0b037acf
pjpolley <pjpolley@wpi.edu>
2017-02-27 17:11:22 -0500

    Fixed compiler error pt1

--- src/Application/Events/StepChangedEvent.java
+++ src/Application/Events/StepChangedEvent.java
@@ -1,10 +1,6 @@
 package Application.Events;
 
-<<<<<<< HEAD:src/Controller/Map/ViewElements/Events/StepChangedEvent.java
-import Entity.Navigation.DirectionFloorStep;
-=======
-import Map.Navigation.DirectionStep;
->>>>>>> ed08e8acc4874f1f2ac5a418bf8ddd41dc2685f1:src/Application/Events/StepChangedEvent.java
+import Map.Navigation.DirectionFloorStep;
 
 /**
  * Created by benjaminhylak on 2/9/17.
