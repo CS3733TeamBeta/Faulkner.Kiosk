ef02b6131629b21e53f61e8b76dd7d77ff938eb5
iancj97 <icjohnson@wpi.edu>
2017-03-01 00:38:13 -0500

    Update Guidance.java
    Removed a cast

--- src/Map/Navigation/Guidance.java
+++ src/Map/Navigation/Guidance.java
@@ -525,7 +525,7 @@ public class Guidance extends Path {
 
     public boolean sendEmailGuidance(String address) {
         String subjectLine;
-        String directionLine = "<H1><center> You have chosen to navigate to " + ((Destination) pathNodes.get(pathNodes.size() - 1)).toString() + ".</center></H1>" + "<H4>";
+        String directionLine = "<H1><center> You have chosen to navigate to " + (pathNodes.get(pathNodes.size() - 1)).toString() + ".</center></H1>" + "<H4>";
         subjectLine = "Your Directions are Enclosed - Faulkner Hospital";
 
         int stepNumber = 1;
