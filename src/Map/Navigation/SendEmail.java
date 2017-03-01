23cbfa8c0dddf0174d10500d413f417de5d555bc
Ian Johnson <icjohnson@wpi.edu>
2017-02-28 20:46:11 -0500

    Works with many floors

--- src/Map/Navigation/SendEmail.java
+++ src/Map/Navigation/SendEmail.java
@@ -76,11 +76,29 @@ public class SendEmail {
 
             for (int i = 1; i <= numDirectionFloors; i++) {
                 String tempString = "<img src =\"cid:imageDirections" + i + "\">" ;
-                tempString = "<tempString>" + "<body> <style>"
-                        + "body{background:#f2f7ff} </style>"
-                        + "<p>...</p> </body> </html>";
+               //tempString = "<tempString>" + "<body> <style>" + "body{background:#f2f7ff} </style>" + "<p>...</p> </body> </html>";
                 imageDirectionsPortion = imageDirectionsPortion + tempString;
             }
+            /*
+            for (int i = 1; i <= numDirectionFloors; i++) {
+                try {
+                    bimg = ImageIO.read(new File("combined" + i + ".png"));
+                    convertToJpg("combined" + i);
+                    width = bimg.getWidth();
+                    height = bimg.getHeight();
+                    bimg = ImageIO.read(new File("combined" + i + ".jpg"));
+                    encoding = SendEmail.encodeToString(bimg);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                String tempString = "";
+                if (!isPhone) {
+                    tempString += "<img src =\"cid:imageDirections" + i + "\" width = \"" + width + "\" height = \"" + height + "\" border = \"0\" />";
+                }
+                //tempString += "<img src=\"data:image/jpg;base64," + encoding + "\" />" + htmlText;
+                imageDirectionsPortion = imageDirectionsPortion + tempString;
+            } */
 
             if (includeImage) {
                 htmlText = "<img src=\"cid:imageLogo\">" + this.message + imageDirectionsPortion;
@@ -102,7 +120,7 @@ public class SendEmail {
 
             multipart.addBodyPart(messageBodyPart);
             if (includeImage) {
-                for (int i = 0; i < numDirectionFloors; i++) {
+                for (int i = 1; i <= numDirectionFloors; i++) {
                     messageBodyPart = new MimeBodyPart();
                     fds = new FileDataSource(
                             "combined" + i + ".png");
